10f12515e70b05d133c1a2779fc145ad1e613e64
Daniil <mytasmic@gmail.com>
2019-06-26 23:26:27 +0300

    basic error processing

--- concurrentTasks.go
+++ concurrentTasks.go
@@ -2,63 +2,118 @@ package main
 
 
 import (
-    "fmt"
+    "log"
     "time"
     "sync"
+    "errors"
 )
 
 
-func runConcurrentTasks(tasks []func() error, concurrency, errs int) {
+func resolveTask(
+    f func() error, taskIdxs chan int,
+    errsCount *int, mx *sync.Mutex,
+    wg *sync.WaitGroup,
+) {
+    if err := f(); err != nil {
+        mx.Lock()
+        *errsCount++
+        mx.Unlock()
+    }
+    <- taskIdxs
+    wg.Done()
+}
+
+
+func runConcurrentTasks(tasks []func() error, concurrency, maxErrs int) {
     if concurrency < 1 {
+        log.Println("Zero and bellow concurrency makes no sense, quiting.")
         return
     }
 
     taskIdxs := make(chan int, concurrency)
+    errsCount := 0
     var wg sync.WaitGroup
+    var mutex sync.Mutex
 
     for i, t := range tasks {
         taskIdxs <- i
-        wg.Add(1)
-        go func(f func() error) {
-            f()
-            defer wg.Done()
-            <- taskIdxs
-        }(t)
+        if errsCount > maxErrs {
+            log.Println("Got", errsCount, "errors, soft stop.")
+            return
+        } else {
+            wg.Add(1)
+            go resolveTask(t, taskIdxs, &errsCount, &mutex, &wg)
+        }
     }
     wg.Wait()
+    log.Println("All tasks are done, got", errsCount, "errors.")
 }
 
 
 func main() {
-    fmt.Println("Start")
+    log.Println("Start")
 
     tasks := []func() error {
         func() error {
-            fmt.Println("1, start", time.Now())
-            time.Sleep(1*time.Second)
-            fmt.Println("1, end", time.Now())
+            log.Println("1, start")
+            time.Sleep(6*time.Second)
+            log.Println("1, end")
             return nil
         },
         func() error {
-            fmt.Println("2, start", time.Now())
+            log.Println("2, start")
             time.Sleep(2*time.Second)
-            fmt.Println("2, end", time.Now())
+            log.Println("2, end")
             return nil
         },
         func() error {
-            fmt.Println("3, start", time.Now())
+            log.Println("3, start")
             time.Sleep(3*time.Second)
-            fmt.Println("3, end", time.Now())
+            log.Println("3, end")
             return nil
         },
         func() error {
-            fmt.Println("4, start", time.Now())
+            log.Println("4, start")
             time.Sleep(4*time.Second)
-            fmt.Println("4, end", time.Now())
+            log.Println("4, end")
+            return nil
+        },
+        func() error {
+            log.Println("5, start")
+            time.Sleep(1*time.Second)
+            log.Println("5, end")
+            err := errors.New("emit macho dwarf: elf header corrupted")
+            return err
+        },
+        func() error {
+            log.Println("6, start")
+            time.Sleep(1*time.Second)
+            log.Println("6, end")
+            err := errors.New("emit macho dwarf: elf header corrupted")
+            return err
+        },
+        func() error {
+            log.Println("7, start")
+            time.Sleep(1*time.Second)
+            log.Println("7, end")
+            err := errors.New("emit macho dwarf: elf header corrupted")
+            return err
+        },
+        func() error {
+            log.Println("8, start")
+            time.Sleep(1*time.Second)
+            log.Println("8, end")
+            err := errors.New("emit macho dwarf: elf header corrupted")
+            return err
+        },
+        func() error {
+            log.Println("9, start")
+            time.Sleep(1*time.Second)
+            log.Println("9, end")
             return nil
         },
     }
 
     runConcurrentTasks(tasks, 3, 1)
-    fmt.Println("End")
+    log.Println("End")
 }
